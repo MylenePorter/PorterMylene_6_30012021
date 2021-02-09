0722ca831cae5f488915b4ddd4b7b65f064a05b1
MylenePorter <mylene.porter@outlook.fr>
2021-02-09 18:43:55 +0100

    🚧 Folder MODELS

---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+//Plugins :
+const mongoose = require('mongoose');
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    }
+});
+
+userSchema.plugin(mongooseUniqueValidator);
+module.exports = mongoose.model('user', userSchema);
+
